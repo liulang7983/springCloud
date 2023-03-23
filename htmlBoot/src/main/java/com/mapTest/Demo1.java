434a3b6dea1e55995430429ad8f76e6924c46341
ming.li <ming.li@goupwith.com>
2023-03-23 22:29:38 +0800

    提交sql

---
+++ htmlBoot/src/main/java/com/mapTest/Demo1.java
@@ -0,0 +1,15 @@
+package com.mapTest;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author ming.li
+ * @date 2023/3/23 18:31
+ */
+public class Demo1 {
+    public static void main(String[] args) {
+        Map map=new HashMap();
+        map.put("s","s");
+    }
+}
